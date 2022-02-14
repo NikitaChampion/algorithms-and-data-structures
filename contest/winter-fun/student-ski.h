42bc8f5fea153f39bd2ce7ae1ad51e6c86ac48c8
Nikita Igumnov <nkigumnov@yandex.ru>
2022-02-14 13:58:44 +0300

    Update student-ski.h

--- contest/winter-fun/student-ski.h
+++ contest/winter-fun/student-ski.h
@@ -29,9 +29,9 @@ private:
 int compare(const Student &student, const Ski &ski) {
     if (ski.length_ < student.height_ + 5) {
         return -1;
-	}
+    }
     if (ski.length_ > student.height_ + 5) {
         return 1;
-	}
+    }
     return 0;
 }
