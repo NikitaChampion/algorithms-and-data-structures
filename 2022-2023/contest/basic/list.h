519d4f2c7e8b389eac21977928012aa380b2e6b7
Nikita Igumnov <nkigumnov@yandex.ru>
2022-09-25 15:09:31 +0300

    Update list.h

--- 2022-2023/contest/basic/list.h
+++ 2022-2023/contest/basic/list.h
@@ -10,21 +10,15 @@ public:
 class List {
 public:
     List();
-
     List(int* values, size_t size);
-
     ~List();
 
     void pushBack(int value);
-
     void pushFront(int value);
-
     int pop();
-
     int pop(size_t position);
-
     void push(int value, size_t position);
 
-    Node* head;
-    Node* tail;
+    Node* head{};
+    Node* tail{};
 };
