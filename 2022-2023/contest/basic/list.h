1a602c5f5fdd01f2e9bc4fd31520b2ecca6ea903
Nikita Igumnov <nkigumnov@yandex.ru>
2022-09-25 10:48:10 +0300

    Create list.h

---
+++ 2022-2023/contest/basic/list.h
@@ -0,0 +1,30 @@
+class Node {
+public:
+    int data{};
+    Node *next, *previous;
+ 
+	Node();
+};
+ 
+class List {
+public:
+    List();
+
+    List(int* values, size_t size);
+
+    ~List();
+
+    void pushBack(int value);
+
+    void pushFront(int value);
+
+    int pop();
+
+    int pop(size_t position);
+
+    void push(int value, size_t position);
+
+    Node* head;
+
+    Node* tail;
+};
