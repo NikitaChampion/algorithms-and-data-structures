85a0ab03107e208b700850e33a4a3da661e633b9
Nikita Igumnov <nkigumnov@yandex.ru>
2022-12-07 04:48:23 +0300

    Create rb-tree.h

---
+++ 2022-2023/contest/forest/rb-tree.h
@@ -0,0 +1,68 @@
+#include <iostream>
+
+enum Color { RED, BLACK };
+
+template <typename T>
+struct Node {
+    T value;
+    Node<T> *left;
+    Node<T> *right;
+    Node<T> *parent;
+    Color color;
+
+    Node();
+    explicit Node(const T& x);
+};
+
+template <class ValueType>
+class RBTree {
+public:
+    struct Iterator {
+        Iterator();
+        explicit Iterator(Node<ValueType>* node);
+
+        const ValueType& operator*() const;
+        const ValueType* operator->() const;
+
+        Iterator& operator++();
+        Iterator operator++(int);
+
+        Iterator& operator--();
+        Iterator operator--(int);
+
+        bool operator==(const Iterator& other) const;
+        bool operator!=(const Iterator& other) const;
+
+    private:
+        // your code
+    };
+
+    RBTree();
+
+    RBTree(const RBTree &other);
+
+    RBTree<ValueType> &operator=(const RBTree &other);
+
+    ~RBTree();
+
+    void insert(const ValueType &value);
+
+    void erase(const ValueType &value);
+
+    size_t size() const;
+
+    bool empty() const;
+
+    ValueType *lowerBound(const ValueType &value) const;
+
+    ValueType *find(const ValueType &value) const;
+
+    Iterator begin() const;
+ 
+    Iterator end() const;
+
+    Node<ValueType> *root;
+
+private:
+    // your code
+};
