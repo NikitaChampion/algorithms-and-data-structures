2677129e157cbefea6986eeee61ac3491a273203
Nikita Igumnov <nkigumnov@yandex.ru>
2022-12-12 16:21:30 +0300

    Create rb-tree-tester.h

---
+++ 2022-2023/contest/forest/rb-tree-tester.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+template <template <typename ValueType> class RedBlackTree>
+class RBTreeTester {
+public:
+    static std::string checkTree();
+
+private:
+    // your code
+};
